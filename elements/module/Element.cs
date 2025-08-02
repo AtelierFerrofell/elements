70816be8106374d3d9b69a0854da4dce377a09b0
Atelier Ferrofell <kai@ferrofell.com>
2025-08-01 22:24:18 -0700

    Abstract interaction class

--- elements/module/Element.cs
+++ elements/module/Element.cs
@@ -8,5 +8,5 @@ public partial class Element : Resource
 	public string Name { get; set; }
 	
 	[Export]
-	public Godot.Collections.Dictionary<Element, int> Interactions { get; set; }
+	public Godot.Collections.Dictionary<Element, Interaction> Interactions { get; set; }
 }
